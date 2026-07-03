77ee66c782c8536e8818adefcd44da2b5e8366e8
Bill.K.Tsui <bill.k.tsui@gmail.com>
2026-07-03 20:16:15 +0800

    🐛 Fix a bug

--- Mosfet.NeteaseMusic/NeteaseMusic.cs
+++ Mosfet.NeteaseMusic/NeteaseMusic.cs
@@ -98,7 +98,6 @@ public class NeteaseMusic
         string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
             ts.Hours, ts.Minutes, ts.Seconds,
             ts.Milliseconds / 10);
-        Console.ForegroundColor = ConsoleColor.Green;
         Console.WriteLine($"已完成！用时 {elapsedTime}");
     }
 
